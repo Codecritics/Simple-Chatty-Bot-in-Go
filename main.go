a0d875de73842e1b607ff1017d16d0703feba5b3
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-05-17 10:14:35 +0200

    stage 3/5: Guess the age

--- main.go
+++ main.go
@@ -3,6 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello! My name is Marius.")
-	fmt.Println("I was created in 2022.")
+	var name string
+	fmt.Println("Hello! My name is Aid.")
+	fmt.Println("I was created in 2020.")
+	fmt.Println("Please, remind me your name.")
+
+	_, err := fmt.Scan(&name)
+	if err != nil {
+		return
+	}
+
+	fmt.Println("What a great name you have, " + name + "!")
 }
